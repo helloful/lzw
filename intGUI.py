29b4bf292c2f0e3e9d7e6dd7a325df5cf0275d40
helloful <42793582+helloful@users.noreply.github.com>
2021-01-17 14:43:22 +0800

    Update intGUI.py

--- intGUI.py
+++ intGUI.py
@@ -18,53 +18,7 @@ dll = cdll.LoadLibrary(r"/usr/local/src/python_code/Linux_aitalk_exp1227_5f0fc56
 in_path= "input1.wav"
 login_params = b"appid = 5f0fc56a, work_dir = Linux_aitalk_exp1227_5f0fc56a"
 START_AUDIO=False
-def get_audio(filepath):
-    # aa = str(input("是否开始录音？   （1是/0否）"))
-    if START_AUDIO==True :
-        CHUNK = 256
-        FORMAT = pyaudio.paInt16
-        CHANNELS = 1                # 声道数
-        RATE = 11025                # 采样率
-        RECORD_SECONDS = 5
-        WAVE_OUTPUT_FILENAME = filepath
-        p = pyaudio.PyAudio()
 
-        stream = p.open(format=FORMAT,
-                        channels=CHANNELS,
-                        rate=RATE,
-                        input=True,
-                        frames_per_buffer=CHUNK)
-
-        print("*"*10, "开始录音：请在5秒内输入语音")
-        frames = []
-        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-            data = stream.read(CHUNK)
-            frames.append(data)
-        print("*"*10, "录音结束\n")
-
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
-
-        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
-        wf.close()
-
-
-def XF_text(filepath, audio_rate):
-    msp = Msp()
-    #print("登录科大讯飞")
-    msp.login()
-    #print("科大讯飞登录成功")
-    session_begin_params = b"sub = iat, ptt = 0, result_encoding = utf8, result_type = plain, domain = iat"
-    if 16000 == audio_rate:
-        session_begin_params = b"sub = iat, domain = iat, language = zh_cn, accent = mandarin, sample_rate = 16000, result_type = plain, result_encoding = utf8"
-    text = msp.isr(filepath, session_begin_params)
-    msp.logout()
-    return text
 class Msp:
     def __init__(self):
         pass
@@ -117,22 +71,69 @@ class Msp:
 
 
 
-def string_client(text):
-    rospy.init_node('string_client')
-    rospy.wait_for_service('print_string')
-    rospy.set_param('truth', text)
-    string_client = rospy.ServiceProxy('print_string',SetBool)
-    response = string_client(True)
-    return response.success, response.message
 
 class Client():
     def __init__(self):
+        self.START_AUDIO=False
         self.initGUI()
         self.initThread()
+
+    def string_client(self,text):
+        rospy.init_node('string_client')
+        rospy.wait_for_service('print_string')
+        rospy.set_param('truth', text)
+        string_client = rospy.ServiceProxy('print_string', SetBool)
+        response = string_client(True)
+        return response.success, response.message
+    def XF_text(self,filepath, audio_rate):
+        msp = Msp()
+        # print("登录科大讯飞")
+        msp.login()
+        # print("科大讯飞登录成功")
+        session_begin_params = b"sub = iat, ptt = 0, result_encoding = utf8, result_type = plain, domain = iat"
+        if 16000 == audio_rate:
+            session_begin_params = b"sub = iat, domain = iat, language = zh_cn, accent = mandarin, sample_rate = 16000, result_type = plain, result_encoding = utf8"
+        text = msp.isr(filepath, session_begin_params)
+        msp.logout()
+        return text
+    def get_audio(self,filepath):
+        # aa = str(input("是否开始录音？   （1是/0否）"))
+        if self.START_AUDIO == True:
+            CHUNK = 256
+            FORMAT = pyaudio.paInt16
+            CHANNELS = 1  # 声道数
+            RATE = 11025  # 采样率
+            RECORD_SECONDS = 5
+            WAVE_OUTPUT_FILENAME = filepath
+            p = pyaudio.PyAudio()
+
+            stream = p.open(format=FORMAT,
+                            channels=CHANNELS,
+                            rate=RATE,
+                            input=True,
+                            frames_per_buffer=CHUNK)
+
+            print("*" * 10, "开始录音：请在5秒内输入语音")
+            frames = []
+            for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+                data = stream.read(CHUNK)
+                frames.append(data)
+            print("*" * 10, "录音结束\n")
+
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
+
+            wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(p.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
+            wf.writeframes(b''.join(frames))
+            wf.close()
     def gameStart(self):
-        global START_AUDIO
-        START_AUDIO=True
-        print("录音开始")
+        self.START_AUDIO=True
+        self.audio.start()
+
 
     def gameOver(self):
         self.window.quit()
@@ -205,16 +206,14 @@ class Client():
 
         self.window.mainloop()
     def mymain(self):
-        while True:
-            get_audio(in_path)
-            text = XF_text(in_path, 16000)
-            # string_client(text)
+        self.get_audio(in_path)
+        text = self.XF_text(in_path, 16000)
+        self.string_client(text)
     def initThread(self):
         self.guiThread = threading.Thread(target=self.initGUI)
         self.audio = threading.Thread(target=self.mymain)
     def start(self):
         self.guiThread.start()
-        self.audio.start()
         # self.mymain()
 
         # self.audio.start()
